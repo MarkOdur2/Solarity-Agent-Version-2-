00814fa97366aefaf93ca8f92703812468d49b34
MarkOdur2 <diblonguganda@gmail.com>
2026-03-05 15:04:39 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SolarityAgent from './App.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SolarityAgent />
+  </React.StrictMode>
+)
